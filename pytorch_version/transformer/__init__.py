e9cfbd3aebd4925960bb5067b0caaa46912008cf
H <1434579002@qq.com>
2018-12-17 17:20:48 +0800

    Initial commit

---
+++ pytorch_version/transformer/__init__.py
@@ -0,0 +1,13 @@
+import pytorch_version.transformer.Constants
+import pytorch_version.transformer.Modules
+import pytorch_version.transformer.Layers
+import pytorch_version.transformer.SubLayers
+import pytorch_version.transformer.Models
+import pytorch_version.transformer.Translator
+import pytorch_version.transformer.Beam
+import pytorch_version.transformer.Optim
+
+__all__ = [
+    pytorch_version.transformer.Constants, pytorch_version.transformer.Modules, pytorch_version.transformer.Layers,
+    pytorch_version.transformer.SubLayers, pytorch_version.transformer.Models, pytorch_version.transformer.Optim,
+    pytorch_version.transformer.Translator, pytorch_version.transformer.Beam]
