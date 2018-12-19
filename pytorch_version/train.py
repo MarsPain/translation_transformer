0665a7f64da0ce71b7bf76c7f8232cdb053eb5ab
H <1434579002@qq.com>
2018-12-19 11:36:39 +0800

    解决错误地共享encoder和decoder中两种语言的嵌入矩阵参数的BUG

--- pytorch_version/train.py
+++ pytorch_version/train.py
@@ -209,7 +209,7 @@ def main():
     parser.add_argument('-n_warmup_steps', type=int, default=4000)
 
     parser.add_argument('-dropout', type=float, default=0.1)
-    parser.add_argument('-embs_share_weight', action='store_true')
+    parser.add_argument('-embs_share_weight', action='store_true', default=False)
     parser.add_argument('-proj_share_weight', action='store_true')
 
     parser.add_argument('-log', default=None)
