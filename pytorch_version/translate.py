e432f89b64cbface574e906dbb59883ffd730faf
H <1434579002@qq.com>
2018-12-23 22:19:37 +0800

    跑通预测程序

--- pytorch_version/translate.py
+++ pytorch_version/translate.py
@@ -14,23 +14,23 @@ def main():
 
     parser = argparse.ArgumentParser(description='translate.py')
 
-    parser.add_argument('-model', required=True,
+    parser.add_argument('-model', required=False, default="ckpt.chkpt",
                         help='Path to model .pt file')
-    parser.add_argument('-src', required=True,
+    parser.add_argument('-src', required=False, default="data/test.en",
                         help='Source sequence to decode (one line per sequence)')
-    parser.add_argument('-vocab', required=True,
+    parser.add_argument('-vocab', required=False, default="train_val_dict.pkl",
                         help='Source sequence to decode (one line per sequence)')
     parser.add_argument('-output', default='pred.txt',
                         help="""Path to output the predictions (each line will
                         be the decoded sequence""")
     parser.add_argument('-beam_size', type=int, default=5,
                         help='Beam size')
-    parser.add_argument('-batch_size', type=int, default=30,
+    parser.add_argument('-batch_size', type=int, default=4,
                         help='Batch size')
     parser.add_argument('-n_best', type=int, default=1,
                         help="""If verbose is set, will output the n_best
                         decoded sentences""")
-    parser.add_argument('-no_cuda', action='store_true')
+    parser.add_argument('-no_cuda', action='store_true', default=True)
 
     opt = parser.parse_args()
     opt.cuda = not opt.no_cuda
@@ -56,12 +56,14 @@ def main():
 
     translator = Translator(opt)
 
-    with open(opt.output, 'w') as f:
+    with open(opt.output, 'w', encoding="utf-8") as f:
         for batch in tqdm(test_loader, mininterval=2, desc='  - (Test)', leave=False):
             all_hyp, all_scores = translator.translate_batch(*batch)
+            # print("all_hyp:", all_hyp)
             for idx_seqs in all_hyp:
                 for idx_seq in idx_seqs:
                     pred_line = ' '.join([test_loader.dataset.tgt_idx2word[idx] for idx in idx_seq])
+                    print("pred_line:", pred_line)
                     f.write(pred_line + '\n')
     print('[Info] Finished.')
 
